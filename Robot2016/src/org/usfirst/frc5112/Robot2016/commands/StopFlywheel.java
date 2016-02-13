51899193e660c176fe4887ce8aa20a2308c362dd
Kristopher Hind <kristopherhind18@PHS326_3946_.FG.local>
2016-02-13 11:59:27 -0500

    add to stop fly wheel command

--- Robot2016/src/org/usfirst/frc5112/Robot2016/commands/StopFlywheel.java
+++ Robot2016/src/org/usfirst/frc5112/Robot2016/commands/StopFlywheel.java
@@ -1,36 +1,33 @@
 package org.usfirst.frc5112.Robot2016.commands;
 
+import org.usfirst.frc5112.Robot2016.Robot;
+
 import edu.wpi.first.wpilibj.command.Command;
 
 public class StopFlywheel extends Command{
 
 	@Override
 	protected void initialize() {
-		// TODO Auto-generated method stub
-		
+		Robot.shooter.stopFlyWheel();
 	}
 
 	@Override
 	protected void execute() {
-		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
 	protected boolean isFinished() {
-		// TODO Auto-generated method stub
-		return false;
+		return true;
 	}
 
 	@Override
 	protected void end() {
-		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
 	protected void interrupted() {
-		// TODO Auto-generated method stub
 		
 	}
 
