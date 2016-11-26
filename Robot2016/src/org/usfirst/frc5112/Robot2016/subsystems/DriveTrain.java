efbf490ea366e1f935364e80e5296a7693991e68
Kyle Corry <kylecorry31@gmail.com>
2016-11-26 11:08:06 -0500

    Update gongoliers.jar
    Implement JoystickTransformer

--- Robot2016/src/org/usfirst/frc5112/Robot2016/subsystems/DriveTrain.java
+++ Robot2016/src/org/usfirst/frc5112/Robot2016/subsystems/DriveTrain.java
@@ -5,6 +5,10 @@ package org.usfirst.frc5112.Robot2016.subsystems;
 import org.usfirst.frc5112.Robot2016.Robot;
 import org.usfirst.frc5112.Robot2016.RobotMap;
 import org.usfirst.frc5112.Robot2016.commands.*;
+
+import com.thegongoliers.input.JoystickTransformer;
+import com.thegongoliers.util.Position;
+
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.RobotDrive;
 import edu.wpi.first.wpilibj.command.PIDSubsystem;
@@ -24,7 +28,7 @@ public class DriveTrain extends PIDSubsystem {
 		getPIDController().setContinuous(false);
 		setOutputRange(-1, 1);
 	}
-	
+
 	public void initDefaultCommand() {
 		setDefaultCommand(new OperateDriveTrain());
 	}
@@ -40,14 +44,18 @@ public class DriveTrain extends PIDSubsystem {
 	 *            The Joystick that will be used to operate the drivetrain.
 	 */
 	public void operate(Joystick stick) {
-//		robotDrive.arcadeDrive(stick.getY() * 0.4, stick.getZ() * 0.6);
-		double y = stick.getY();
-		double rotation = stick.getZ();
-		if (Math.abs(rotation) >= 0.1) {
-			robotDrive.arcadeDrive(y * 0.85, 3.0 * Math.copySign(1, stick.getZ()) * Math.pow(stick.getZ(), 2) / 4.0);
+		Position stickPosition = new Position(stick.getX(), stick.getY());
+		JoystickTransformer transformer = new JoystickTransformer();
+		double newY = transformer.sensitivity(transformer.scaledRadialDeadzone(stickPosition, 0.05), 0.85).getY();
+		Position rotation = new Position(stick.getZ(), 0);
+		double newRotation = transformer
+				.sensitivity(transformer.power(transformer.scaledRadialDeadzone(rotation, 0.1), 2), 0.75).getFirst();
+
+		if (newRotation == 0) {
+			robotDrive.arcadeDrive(newY, newRotation);
 			setInitialGyro(Robot.gyro.getAngle());
 		} else {
-			robotDrive.arcadeDrive(y * 0.85, -0.01 * (Robot.gyro.getAngle() - initialGyro));
+			robotDrive.arcadeDrive(newY, -0.01 * (Robot.gyro.getAngle() - initialGyro));
 		}
 	}
 
@@ -98,15 +106,14 @@ public class DriveTrain extends PIDSubsystem {
 		robotDrive.arcadeDrive(0, -rotationSpeed);
 	}
 
-	
 	@Override
 	protected double returnPIDInput() {
 		return Robot.gyro.getAngle();
 	}
-	
+
 	@Override
 	protected void usePIDOutput(double output) {
-		rotateCCW(output);		
+		rotateCCW(output);
 	}
 
 }
